838cba03cd29a3eaeaa1f674ebff8a2fd0707620
Peu.dev <ps309205@gmail.com>
2026-03-05 12:42:34 -0300

    Controller Created

---
+++ src/main/java/dev/jpeu/cadSys/Controller.java
@@ -0,0 +1,18 @@
+package dev.jpeu.cadSys;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Boas Vindas";
+    }
+
+
+
+}
