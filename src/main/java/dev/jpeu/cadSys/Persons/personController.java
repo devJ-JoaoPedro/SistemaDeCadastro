ad45f28bc20d65adad6b8581210610bf3cf3727a
Peu.dev <ps309205@gmail.com>
2026-03-16 11:36:17 -0300

    feat: update controller with data mock

--- src/main/java/dev/jpeu/cadSys/Persons/personController.java
+++ src/main/java/dev/jpeu/cadSys/Persons/personController.java
@@ -1,8 +1,6 @@
 package dev.jpeu.cadSys.Persons;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,6 +11,40 @@ public class personController {
         return "Boas Vindas";
     }
 
+    // Add people (CREATE)
+    @PostMapping("/addPerson")
+    public String addPerson() {
+        return "Person Added Successfully";
+    }
 
+    // Search people for id (CREATE)
+    @GetMapping("/searchPerson")
+    public String searchPeople() {
+        return "Person Found Successfully";
+    }
+
+    // Show all people (READ)
+    @GetMapping("/allPeople")
+    public String allPeople() {
+        return "List With All the People";
+    }
+
+    // Show people for id (READ)
+    @GetMapping("/showPersonById")
+    public String showPersonById() {
+        return "Person Found Successfully";
+    }
+
+    // Modify people (UPDATE)
+    @PutMapping("/modifyById")
+    public String modifyPersonById() {
+        return "Person Modified Successfully";
+    }
+
+    // Delete People (DELETE)
+    @DeleteMapping("/deleteById")
+    public String deletePersonById() {
+        return "Person Deleted Successfully";
+    }
 
 }
